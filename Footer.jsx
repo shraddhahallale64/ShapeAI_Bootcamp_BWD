b9c8c231ec846203c542bb2f5b77d391829a8611
shraddhahallale64 <85128655+shraddhahallale64@users.noreply.github.com>
2021-06-07 11:27:19 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Copyright@2021</h1>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        {" "}
+        This was an amazing Bootcamp taken by Shaurya Sinha. We coverd
+        everything from scratch including javascript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
